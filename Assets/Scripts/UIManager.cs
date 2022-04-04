444581cd787ff12504f36fa8f9a3dad5cea12dda
Vincent ABEL-GRIMALT <vincent.abel.grimalt@gmail.com>
2022-04-04 16:27:16 +0200

    Adjustments, after pull, and tweaks to ui looks

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -15,7 +15,6 @@ public class UIManager : MonoBehaviour
         /* Here you all CanvasGroup goes to the main Hashtable */
         UIList.Add("Pause", GameObject.Find("Pause").GetComponent<CanvasGroup>());
         UIList.Add("Main", GameObject.Find("MainCanvas").GetComponent<CanvasGroup>());
-        UIList.Add("Obj", GameObject.Find("Upgrades").GetComponent<CanvasGroup>());
         UIList.Add("Lose", GameObject.Find("GameOver").GetComponent<CanvasGroup>());
     }
 
@@ -58,7 +57,6 @@ public class UIManager : MonoBehaviour
                 PauseUnpause();
                 ((CanvasGroup)UIList["Pause"]).alpha = 0;
                 ((CanvasGroup)UIList["Main"]).alpha = 1;
-                ((CanvasGroup)UIList["Obj"]).alpha = 0;
 
             } else {                                            // Pausing Game
                 GameObject.Find("VolumeSlider").GetComponent<Slider>().value = StaticVar.Getvolume();
@@ -67,31 +65,6 @@ public class UIManager : MonoBehaviour
                 PauseUnpause();
                 ((CanvasGroup)UIList["Pause"]).alpha = 1;
                 ((CanvasGroup)UIList["Main"]).alpha = 0;
-                ((CanvasGroup)UIList["Obj"]).alpha = 0;
-            }
-        }
-        if (value == "Obj")
-        {                                   // Display Objectives
-            if (StaticVar.gameIsPaused && (((CanvasGroup)UIList["Obj"]).alpha == 0))
-            { // IF paused and just sumonned
-                ((CanvasGroup)UIList["Pause"]).alpha = 0;
-                ((CanvasGroup)UIList["Main"]).alpha = 1;
-                ((CanvasGroup)UIList["Obj"]).alpha = 1;
-            }
-            else if (!StaticVar.gameIsPaused && (((CanvasGroup)UIList["Obj"]).alpha == 0))
-            { // or just summon but Pause wasn't enforced
-                StaticVar.gameIsPaused = true;
-                PauseUnpause();
-                ((CanvasGroup)UIList["Main"]).alpha = 0;
-                ((CanvasGroup)UIList["Obj"]).alpha = 1;
-            }
-            else if (StaticVar.gameIsPaused && (((CanvasGroup)UIList["Obj"]).alpha == 1))
-            { //Otherwise Just unpause and leave
-                StaticVar.gameIsPaused = false;
-                PauseUnpause();
-                ((CanvasGroup)UIList["Pause"]).alpha = 0;
-                ((CanvasGroup)UIList["Main"]).alpha = 1;
-                ((CanvasGroup)UIList["Obj"]).alpha = 0;
             }
         }
         if (value == "Lose")
@@ -117,14 +90,12 @@ public class UIManager : MonoBehaviour
         if (!StaticVar.Lose)
         {
             if (Input.GetKeyDown(KeyCode.Escape)) Manager("Pause");
-            if (Input.GetKeyDown(KeyCode.F1)) Manager("Obj");
             if (Input.GetKeyDown(KeyCode.F11)) Manager("Lose");
         }
         if (StaticVar.Lose) Manager("Lose");
     }
     public void ResumeGame() => Manager("Pause");
     public void PauseGame() => Manager("Pause");
-    public void ObjPanel() => Manager("Obj");
     public void MainPanel() => Manager("Main");
 
     void PauseUnpause() => Time.timeScale = (StaticVar.gameIsPaused) ? 0 : 1;
